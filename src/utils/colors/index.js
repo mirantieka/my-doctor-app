00f580f592f6469cddf78297afd6817e74dc9d50
mirantieka <mirantiekaa4@gmail.com>
2021-09-02 23:15:25 +0700

    initiate fonts and colors globally

---
+++ src/utils/colors/index.js
@@ -0,0 +1,19 @@
+const mainColors= {
+    green: '#0BCAD4',
+    darkBlue: '#112340',
+    grey: '#7D8797',
+    white: '#ffffff',
+    black: '#000000'
+}
+
+export const colors= {
+    primary: mainColors.green,
+    secondary: mainColors.darkBlue,
+    white: mainColors.white,
+    black: mainColors.black,
+
+    text: {
+        primary: mainColors.darkBlue,
+        secondary: mainColors.grey
+    },
+}
---
+++ src/utils/fonts/index.js
@@ -0,0 +1,17 @@
+export const fonts = {
+    primary: {
+        200: 'Nunito-ExtraLight',
+        300: 'Nunito-Light',
+        400: 'Nunito-Regular',
+        600: 'Nunito-SemiBold',
+        700: 'Nunito-Bold',
+        800: 'Nunito-SemiBold',
+        900: 'Nunito-ExtraBold',
+        extraLight: 'Nunito-ExtraLight',
+        light: 'Nunito-Light',
+        normal: 'Nunito-Regular',
+        semiBold: 'Nunito-SemiBold',
+        bold: 'Nunito-Bold',
+        extraBold: 'Nunito-ExtraBold',
+    }
+}
