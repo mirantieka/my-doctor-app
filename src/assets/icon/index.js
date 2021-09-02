61c7bfb896ddd3a14e7341a99fbca895d04aa2a7
mirantieka <mirantiekaa4@gmail.com>
2021-09-02 23:17:40 +0700

    adding icon

---
+++ src/assets/icon/index.js
@@ -0,0 +1,5 @@
+import IconBackDark from './arrow_back_24px';
+import IconAddPhoto from './btn_add_photo';
+import IconRemovePhoto from './btn_remove_photo';
+
+export {IconBackDark, IconAddPhoto, IconRemovePhoto};
