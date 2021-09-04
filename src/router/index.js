73e17578b3e01fdfc3ede6b3dcddac9df621a33f
mirantieka <mirantiekaa4@gmail.com>
2021-09-04 22:07:54 +0700

    adding route tab navigator

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,31 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
-import {GetStarted, Login, Register, Splash, UploadPhoto} from '../pages';
+import {Doctor, GetStarted, Hospitals, Login, Messages, Register, Splash, UploadPhoto} from '../pages';
+import BottomNavigator from '../components/complex/BottomNavigator';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator tabBar={props => <BottomNavigator {...props} />}>
+      <Tab.Screen name="Doctor" component={Doctor} options={{headerShown: false}} />
+      <Tab.Screen name="Messages" component={Messages} options={{headerShown: false}} />
+      <Tab.Screen name="Hospitals" component={Hospitals} options={{headerShown: false}} />
+    </Tab.Navigator>
+  )
+}
 
 const Router = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="MainApp">
       <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}} />
       <Stack.Screen name="GetStarted" component={GetStarted} options={{headerShown: false}} />
       <Stack.Screen name="Register" component={Register} options={{headerShown: false}} />
       <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
       <Stack.Screen name="UploadPhoto" component={UploadPhoto} options={{headerShown: false}} />
+      <Stack.Screen name="MainApp" component={MainApp} options={{headerShown: false}} />
     </Stack.Navigator>
   );
 };
