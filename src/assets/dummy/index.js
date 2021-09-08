1f9daa1499ae2bdb4e3c9ad8a29530a8989a7e88
mirantieka <mirantiekaa4@gmail.com>
2021-09-08 17:00:48 +0700

    adding new assets

---
+++ src/assets/dummy/index.js
@@ -0,0 +1,29 @@
+import DummyUser from './user_pic.png';
+import DummyDoctor from './doc1.png';
+import DummyDoctor2 from './doc2.png';
+import DummyDoctor3 from './doc3.png';
+import DummyDoctor4 from './user_doc.png';
+import DummyDoctor5 from './user_doc_1.png';
+import DummyDoctor6 from './user_doc_2.png';
+import DummyNews from './pic.png';
+import DummyNews2 from './pic_news_2.png';
+import DummyNews3 from './pic_news_3.png';
+import DummyHospital1 from './pic_hospital.png';
+import DummyHospital2 from './pic_hospital_2.png';
+import DummyHospital3 from './pic_hospital_3.png';
+
+export {
+  DummyUser,
+  DummyDoctor,
+  DummyDoctor2,
+  DummyDoctor3,
+  DummyDoctor4,
+  DummyDoctor5,
+  DummyDoctor6,
+  DummyNews,
+  DummyNews2,
+  DummyNews3,
+  DummyHospital1,
+  DummyHospital2,
+  DummyHospital3,
+};
--- src/assets/icon/index.js
+++ src/assets/icon/index.js
@@ -1,4 +1,5 @@
 import IconBackDark from './arrow_back_24px';
+import IconBackLight from './arrow_back_light_24px';
 import IconAddPhoto from './btn_add_photo';
 import IconRemovePhoto from './btn_remove_photo';
 import IconDoctorActive from './face_active_24px';
@@ -7,9 +8,14 @@ import IconMessagesActive from './textsms_active_24px';
 import IconMessagesInactive from './textsms_24px';
 import IconHospitalsActive from './map_active_24px';
 import IconHospitalsInactive from './map_24px';
+import IconStar from './star_rate_18px.svg'
+import IconNext from './chevron_right_24px_outlined'
+import IconSend from './send_24px.svg'
+import IconSendDisable from './send_24px_disable.svg'
 
 export {
   IconBackDark,
+  IconBackLight,
   IconAddPhoto,
   IconRemovePhoto,
   IconDoctorActive,
@@ -17,5 +23,9 @@ export {
   IconMessagesInactive,
   IconMessagesActive,
   IconHospitalsActive,
-  IconHospitalsInactive
+  IconHospitalsInactive,
+  IconStar,
+  IconNext,
+  IconSend,
+  IconSendDisable
 };
