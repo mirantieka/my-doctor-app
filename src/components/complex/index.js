f6768e8446c8611cb5c0ffa769efb102f85c52bc
mirantieka <mirantiekaa4@gmail.com>
2021-09-08 17:03:27 +0700

    update export complex components

--- src/components/complex/index.js
+++ src/components/complex/index.js
@@ -1,4 +1,23 @@
-import Header from "./Header";
-import BottomNavigator from "./BottomNavigator";
+import Header from './Header';
+import BottomNavigator from './BottomNavigator';
+import UserProfile from './UserProfile';
+import DoctorCategory from './DoctorCategory';
+import DoctorRated from './DoctorRated';
+import NewsItem from './NewsItem';
+import ListDoctor from './ListDoctor';
+import ListHospital from './ListHospital';
+import ChatItem from './ChatItem';
+import InputChat from './InputChat';
 
-export {Header, BottomNavigator}
+export {
+  Header,
+  BottomNavigator,
+  UserProfile,
+  DoctorCategory,
+  DoctorRated,
+  NewsItem,
+  ListDoctor,
+  ListHospital,
+  ChatItem,
+  InputChat
+};
