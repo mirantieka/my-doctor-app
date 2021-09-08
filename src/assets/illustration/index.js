e49d48ad47f4c69f0c823bb021813ed14689e55c
mirantieka <mirantiekaa4@gmail.com>
2021-09-08 17:05:40 +0700

    update export illustration

--- src/assets/illustration/index.js
+++ src/assets/illustration/index.js
@@ -1,5 +1,19 @@
-import ILlogo from './logo.svg'
-import ILGetStarted from './bg.png'
-import ILNullPhoto from './user_photo_null.png'
+import ILlogo from './logo.svg';
+import ILGetStarted from './bg.png';
+import ILNullPhoto from './user_photo_null.png';
+import ILDokterUmum from './icon_dokter_umum.png';
+import ILPsikiater from './icon_psikiater.png';
+import ILDokterAnak from './icon_dokter_anak.png';
+import ILDokterObat from './icon_obat.png';
+import ILHospitalBackground from './cover.png';
 
-export {ILlogo, ILGetStarted, ILNullPhoto};
+export {
+  ILlogo,
+  ILGetStarted,
+  ILNullPhoto,
+  ILDokterUmum,
+  ILHospitalBackground,
+  ILPsikiater,
+  ILDokterAnak,
+  ILDokterObat
+};
