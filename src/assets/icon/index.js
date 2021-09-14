0232f17211c4c9e8f44fdb5fca867045dc89881e
mirantieka <mirantiekaa4@gmail.com>
2021-09-14 13:32:01 +0700

    adding new icons

--- src/assets/icon/index.js
+++ src/assets/icon/index.js
@@ -12,6 +12,10 @@ import IconStar from './star_rate_18px.svg'
 import IconNext from './chevron_right_24px_outlined'
 import IconSend from './send_24px.svg'
 import IconSendDisable from './send_24px_disable.svg'
+import IconAccount from './account_circle_24px.svg'
+import IconLanguage from './translate_24px.svg'
+import IconRate from './star_outline_24px.svg'
+import IconHelp from './description_24px.svg'
 
 export {
   IconBackDark,
@@ -27,5 +31,9 @@ export {
   IconStar,
   IconNext,
   IconSend,
-  IconSendDisable
+  IconSendDisable,
+  IconAccount,
+  IconLanguage,
+  IconRate,
+  IconHelp
 };
