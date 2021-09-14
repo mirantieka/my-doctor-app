900493505c585c942304f7e0a177fd92eb6349b1
mirantieka <mirantiekaa4@gmail.com>
2021-09-14 13:35:18 +0700

    adding new routes and colours

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,23 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
-import {ChooseDoctor, Doctor, GetStarted, Hospitals, Login, Messages, Register, Splash, UploadPhoto} from '../pages';
+import {
+  ChooseDoctor,
+  Doctor,
+  DoctorProfile,
+  EditProfile,
+  GetStarted,
+  Hospitals,
+  Login,
+  Messages,
+  Register,
+  Splash,
+  UploadPhoto,
+} from '../pages';
 import BottomNavigator from '../components/complex/BottomNavigator';
 import Chatting from '../pages/Chatting';
+import { UserProfile } from '../components';
+import Profile from '../pages/Profile';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -11,24 +25,83 @@ const Tab = createBottomTabNavigator();
 const MainApp = () => {
   return (
     <Tab.Navigator tabBar={props => <BottomNavigator {...props} />}>
-      <Tab.Screen name="Doctor" component={Doctor} options={{headerShown: false}} />
-      <Tab.Screen name="Messages" component={Messages} options={{headerShown: false}} />
-      <Tab.Screen name="Hospitals" component={Hospitals} options={{headerShown: false}} />
+      <Tab.Screen
+        name="Doctor"
+        component={Doctor}
+        options={{headerShown: false}}
+      />
+      <Tab.Screen
+        name="Messages"
+        component={Messages}
+        options={{headerShown: false}}
+      />
+      <Tab.Screen
+        name="Hospitals"
+        component={Hospitals}
+        options={{headerShown: false}}
+      />
     </Tab.Navigator>
-  )
-}
+  );
+};
 
 const Router = () => {
   return (
     <Stack.Navigator initialRouteName="MainApp">
-      <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}} />
-      <Stack.Screen name="GetStarted" component={GetStarted} options={{headerShown: false}} />
-      <Stack.Screen name="Register" component={Register} options={{headerShown: false}} />
-      <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
-      <Stack.Screen name="UploadPhoto" component={UploadPhoto} options={{headerShown: false}} />
-      <Stack.Screen name="MainApp" component={MainApp} options={{headerShown: false}} />
-      <Stack.Screen name="ChooseDoctor" component={ChooseDoctor} options={{headerShown: false}} />
-      <Stack.Screen name="Chatting" component={Chatting} options={{headerShown: false}} />
+      <Stack.Screen
+        name="Splash"
+        component={Splash}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="GetStarted"
+        component={GetStarted}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Register"
+        component={Register}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="UploadPhoto"
+        component={UploadPhoto}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="MainApp"
+        component={MainApp}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="ChooseDoctor"
+        component={ChooseDoctor}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Chatting"
+        component={Chatting}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Profile"
+        component={Profile}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="EditProfile"
+        component={EditProfile}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="DoctorProfile"
+        component={DoctorProfile}
+        options={{headerShown: false}}
+      />
     </Stack.Navigator>
   );
 };
--- src/utils/colors/index.js
+++ src/utils/colors/index.js
@@ -4,19 +4,36 @@ const mainColors= {
     darkText: '#495A75',
     grey: '#7D8797',
     white: '#ffffff',
-    black: '#000000'
+    black: '#000000',
+    green2: '#EDFCFD',
+    grey2: '#EEEEEE',
+    grey3: '#8092AF',
+    grey4: '#EDEEF0',
+    grey5: '#B1B7C2',
+    blue: '#0066CB'
 }
 
 export const colors= {
     primary: mainColors.green,
     secondary: mainColors.darkBlue,
+    tertiary: mainColors.blue,
+
     white: mainColors.white,
     black: mainColors.black,
+    border: mainColors.grey2,
+    input: mainColors.grey4,
 
     text: {
         primary: mainColors.darkBlue,
         secondary: mainColors.grey,
         menuActive: mainColors.green,
-        menuInactive: mainColors.darkText
+        menuInactive: mainColors.darkText,
+        subTitle: mainColors.grey3,
+        input: mainColors.grey5
     },
+
+    card: {
+        primary: mainColors.green2,
+        secondary: mainColors.green
+    }
 }
