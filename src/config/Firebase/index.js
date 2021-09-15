71892e09abc9e847a048f24c995c8eef80f398e6
mirantieka <mirantiekaa4@gmail.com>
2021-09-15 11:57:14 +0700

    firebase config

---
+++ src/config/Firebase/index.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDxXb0NKGVIyMY2xy3RlBooQr66P8qQhoU',
+  authDomain: 'my-doctor-6de4c.firebaseapp.com',
+  projectId: 'my-doctor-6de4c',
+  storageBucket: 'my-doctor-6de4c.appspot.com',
+  messagingSenderId: '308507027174',
+  appId: '1:308507027174:web:7c8b46bed66cad6d5dfd27',
+});
+
+export default !firebase.apps.length
+  ? firebase.initializeApp(config)
+  : firebase.app();
