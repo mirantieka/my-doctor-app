c75c1426b29efb673a5ece58233c3c4f2b6ebc03
mirantieka <mirantiekaa4@gmail.com>
2021-09-15 11:59:34 +0700

    useState custom

---
+++ src/utils/useForm/index.js
@@ -0,0 +1,14 @@
+import {useState} from 'react';
+
+export const useForm = initialState => {
+  const [values, setValues] = useState(initialState);
+  return [
+    values,
+    (formType, formValue) => {
+      if (formType === 'reset') {
+        return setValues(initialState);
+      }
+      return setValues({...values, [formType]: formValue});
+    },
+  ];
+};
