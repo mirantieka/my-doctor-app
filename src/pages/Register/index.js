75d1016d5e46b67105bcfcb4200e2efc1dcce5fa
mirantieka <mirantiekaa4@gmail.com>
2021-09-15 12:01:06 +0700

    register page sync with firebase and loading component

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,25 +1,77 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Button, Gap, Header, Input } from '../../components';
-import { colors } from '../../utils';
+import React, {useState} from 'react';
+import {ScrollView, StyleSheet, View} from 'react-native';
+import {Button, Gap, Header, Input, Loading} from '../../components';
+import {Firebase} from '../../config';
+import {colors, useForm} from '../../utils';
 
 export default function Register({navigation}) {
+  // const [fullName, setFullName] = useState('');
+  // const [profession, setProfession] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+
+  const [form, setForm] = useForm({
+    fullName: '',
+    profession: '',
+    email: '',
+    password: '',
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const onContinue = () => {
+    console.log(form);
+    setLoading(true);
+    Firebase.auth()
+      .createUserWithEmailAndPassword(form.email, form.password)
+      .then(success => {
+        setLoading(false);
+        setForm('reset');
+        console.log('Register Success: ', success);
+      })
+      .catch(error => {
+        setLoading(false);
+        const errorMessage = error.message;
+        console.log('Register Error: ', errorMessage);
+      });
+    () => navigation.navigate('UploadPhoto');
+  };
   return (
     <View style={styles.page}>
       <View>
-        <Header title="Daftar Akun" onPress={()=>navigation.goBack()} />
+        <Header title="Daftar Akun" onPress={() => navigation.goBack()} />
       </View>
-      <View style={{paddingHorizontal: 40, marginTop: 4}}>
-        <Input title="Full Name" />
-        <Gap height={24} />
-        <Input title="Pekerjaan" />
-        <Gap height={24} />
-        <Input title="Email Address" />
-        <Gap height={24} />
-        <Input title="Password" />
-        <Gap height={40} />
-        <Button title="Continue" onPress={()=> navigation.navigate('UploadPhoto')} />
+      <View style={styles.container}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Input
+            title="Full Name"
+            value={form.fullName}
+            onChangeText={value => setForm('fullName', value)}
+          />
+          <Gap height={24} />
+          <Input
+            title="Pekerjaan"
+            value={form.profession}
+            onChangeText={value => setForm('profession', value)}
+          />
+          <Gap height={24} />
+          <Input
+            title="Email Address"
+            value={form.email}
+            onChangeText={value => setForm('email', value)}
+          />
+          <Gap height={24} />
+          <Input
+            title="Password"
+            value={form.password}
+            onChangeText={value => setForm('password', value)}
+            secureTextEntry
+          />
+          <Gap height={40} />
+          <Button title="Continue" onPress={onContinue} />
+        </ScrollView>
       </View>
+      {loading && <Loading />}
     </View>
   );
 }
@@ -29,4 +81,5 @@ const styles = StyleSheet.create({
     backgroundColor: colors.white,
     flex: 1,
   },
+  container: {paddingHorizontal: 40, marginTop: 4},
 });
