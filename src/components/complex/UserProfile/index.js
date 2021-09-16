b4db8139f7ab94a129bcc42e93ab8108f58133c9
mirantieka <mirantiekaa4@gmail.com>
2021-09-16 16:24:14 +0700

    user profile integration with register and login

--- src/components/complex/UserProfile/index.js
+++ src/components/complex/UserProfile/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import {DummyUser} from '../../../assets';
-import { colors, fonts } from '../../../utils';
+import {ILNullPhoto} from '../../../assets';
+import {colors, fonts} from '../../../utils';
+import {getData} from '../../../utils/localStorage';
 
 export default function UserProfile({onPress}) {
+  const [profile, setProfile] = useState({
+    photo: ILNullPhoto,
+    fullName: '',
+    profession: '',
+  });
+
+  useEffect(() => {
+    getData('user').then(res => {
+      console.log('res: ', res)
+      const data = res;
+      data.photo = {uri: res.photo};
+      console.log('new profile: ', data);
+      setProfile(res);
+    });
+  }, []);
   return (
     <TouchableOpacity style={styles.container} onPress={onPress}>
-      <Image source={DummyUser} style={styles.image} />
+      <Image source={profile.photo} style={styles.image} />
       <View style={styles.textWrapper}>
-        <Text style={styles.name}>Shayna Melinda</Text>
-        <Text style={styles.job}>Product Designer</Text>
+        <Text style={styles.name}>{profile.fullName}</Text>
+        <Text style={styles.job}>{profile.profession}</Text>
       </View>
     </TouchableOpacity>
   );
@@ -19,21 +35,24 @@ const styles = StyleSheet.create({
   image: {
     width: 46,
     height: 46,
+    borderRadius: 46/2
   },
   container: {
     flexDirection: 'row',
   },
   textWrapper: {
-      marginLeft: 12
+    marginLeft: 12,
   },
   name: {
-      fontFamily: fonts.primary.semiBold,
-      fontSize: 16,
-      color: colors.text.primary
+    fontFamily: fonts.primary.semiBold,
+    fontSize: 16,
+    color: colors.text.primary,
+    textTransform: 'capitalize',
   },
   job: {
     fontFamily: fonts.primary.normal,
     fontSize: 12,
-    color: colors.text.secondary
-}
+    color: colors.text.secondary,
+    textTransform: 'capitalize',
+  },
 });
