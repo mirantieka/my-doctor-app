e6cec8790e98defb1f09e12542769647201976a0
mirantieka <mirantiekaa4@gmail.com>
2021-09-16 16:25:05 +0700

    upload photo integration with firebase

--- src/pages/UploadPhoto/index.js
+++ src/pages/UploadPhoto/index.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
-import {IconAddPhoto, ILNullPhoto} from '../../assets';
+import React, {useState} from 'react';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {showMessage} from 'react-native-flash-message';
+import {launchImageLibrary} from 'react-native-image-picker';
+import {IconAddPhoto, IconRemovePhoto, ILNullPhoto} from '../../assets';
 import {Button, Gap, Header, Link} from '../../components';
+import {Firebase} from '../../config';
 import {colors, fonts} from '../../utils';
+import {storeData} from '../../utils/localStorage';
+import base64 from 'react-native-image-base64';
 
-export default function UploadPhoto({navigation}) {
+export default function UploadPhoto({navigation, route}) {
+  const{fullName, profession, uid} = route.params;
+
+  const [hasPhoto, setHasPhoto] = useState(false);
+  const [avatar, setAvatar] = useState(ILNullPhoto);
+  const [photoDB, setPhotoDB] = useState('');
+
+  //Upload Image from gallery device
+  const getImage = () => {
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+      quality: 0.5,
+      maxWidth: 200,
+      maxHeight: 200,
+    };
+    launchImageLibrary(options, response => {
+      if (response.didCancel) {
+        showMessage({
+          message: 'Gagal memuat image',
+          type: 'default',
+          backgroundColor: colors.error,
+          color: colors.white,
+        });
+      } else {
+        const source = {uri: response.assets[0].uri};
+
+        base64
+          .getBase64String(response.assets[0].uri)
+          .then(res=>{
+            console.log('res: ', res)
+            setPhotoDB(`data: ${response.assets[0].type};base64, ${res}`);
+          });
+        
+        setAvatar(source);
+        setHasPhoto(true);
+
+        console.log('response: ', response);
+        
+      }
+    });
+  };
+
+  const uploadPhotoDB = () => {
+    Firebase.database()
+      .ref('users/' + uid + '/')
+      .update({photo: photoDB});
+
+    //store ke localstorage
+    const data = route.params;
+    data.photo = photoDB;
+    storeData('user', data);
+
+    //route
+    navigation.replace('MainApp');
+  };
+
   return (
     <View style={styles.page}>
-      <Header title="Upload Photo" onPress={()=>navigation.goBack()} />
+      <Header title="Upload Photo" onPress={() => navigation.goBack()} />
       <View style={styles.container}>
         <View style={styles.profile}>
-          <View style={styles.photoWrapper}>
-            <Image source={ILNullPhoto} style={styles.addPhoto} />
-            <IconAddPhoto style={styles.btnAddPhoto} />
-          </View>
-          <Text style={styles.name}>Shayna Melinda</Text>
-          <Text style={styles.profesion}>Product Designer</Text>
+          <TouchableOpacity style={styles.photoWrapper} onPress={getImage}>
+            <Image source={avatar} style={styles.addPhoto} />
+            {hasPhoto && <IconRemovePhoto style={styles.btnAddPhoto} />}
+            {!hasPhoto && <IconAddPhoto style={styles.btnAddPhoto} />}
+          </TouchableOpacity>
+          <Text style={styles.name}>{fullName}</Text>
+          <Text style={styles.profesion}>{profession}</Text>
         </View>
         <View style={styles.bottomContainer}>
-          <Button title="Upload and Continue" onPress={()=>navigation.replace('MainApp')} />
+          <Button
+            disable={!hasPhoto}
+            title="Upload and Continue"
+            onPress={uploadPhotoDB}
+          />
           <Gap height={30} />
-          <Link title="Skip for this" textAlign="center" size={16} onPress={()=>navigation.replace('MainApp')} />
+          <Link
+            title="Skip for this"
+            textAlign="center"
+            size={16}
+            onPress={() => navigation.replace('MainApp')}
+          />
         </View>
       </View>
     </View>
@@ -35,6 +108,7 @@ const styles = StyleSheet.create({
   addPhoto: {
     width: 110,
     height: 110,
+    borderRadius: 110 / 2,
   },
   photoWrapper: {
     width: 130,
@@ -54,27 +128,27 @@ const styles = StyleSheet.create({
     paddingHorizontal: 40,
     justifyContent: 'space-between',
     flex: 1,
-    backgroundColor: colors.white
+    backgroundColor: colors.white,
   },
-  bottomContainer:{
-      marginBottom: 64
+  bottomContainer: {
+    marginBottom: 64,
   },
-  profile:{
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: colors.white,
-      flex: 1
+  profile: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.white,
+    flex: 1,
   },
   name: {
-      fontSize: 24,
-      marginTop: 26,
-      fontFamily: fonts.primary.semiBold,
-      color: colors.text.primary
+    fontSize: 24,
+    marginTop: 26,
+    fontFamily: fonts.primary.semiBold,
+    color: colors.text.primary,
   },
   profesion: {
     fontSize: 18,
     marginTop: 4,
     fontFamily: fonts.primary.normal,
-    color: colors.text.secondary
-}
+    color: colors.text.secondary,
+  },
 });
