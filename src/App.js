f5990cfbcc4b37077a86b212645e31080950086e
mirantieka <mirantiekaa4@gmail.com>
2021-09-20 17:07:34 +0700

    redux setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
-import Router from './router';
+import { NavigationContainer } from '@react-navigation/native';
+import React, { useState } from 'react';
 import FlashMessage from 'react-native-flash-message';
+import { Provider, useSelector } from 'react-redux';
+import { Loading } from './components';
+import store from './redux/store';
+import Router from './router';
 
-export default function App() {
-  return (
+const MainApp = () => {
+  
+  const stateGlobal = useSelector(state => state)
+  console.log('state global: ', stateGlobal);
+  return(
     <>
       <NavigationContainer>
         <Router />
       </NavigationContainer>
       <FlashMessage position="top" />
+      {stateGlobal.loading && <Loading/>}
     </>
+  )
+}
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <MainApp />
+    </Provider>
   );
 }
