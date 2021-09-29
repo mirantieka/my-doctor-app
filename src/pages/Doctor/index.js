3e262797a5b10bcba068d85422f1384a7e91c1cb
mirantieka <mirantiekaa4@gmail.com>
2021-09-29 10:46:51 +0700

    intergration Doctor page with firebase

--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -1,58 +1,141 @@
-import React, { useEffect } from 'react';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import {
   DoctorCategory,
   DoctorRated,
   Gap,
   NewsItem,
-  UserProfile,
+  UserProfile
 } from '../../components';
-import {colors, fonts} from '../../utils';
-import { DummyNews, DummyNews2, DummyNews3, JSONCategoryDoctor, JSONRatedDoctor } from '../../assets';
-import { getData } from '../../utils/localStorage';
+import { Firebase } from '../../config';
+import { colors, fonts, showError } from '../../utils';
 
 export default function Doctor({navigation}) {
-  
+  const [news, setNews] = useState([]);
+  const [doctorCtg, setDoctorCtg] = useState([]);
+  const [doctorRated, setDoctorRated] = useState([]);
+
+  useEffect(() => {
+    getCategoryDoctor();
+    getNews();
+    getRatedDoctor();
+  }, []);
+
+  const getNews = () => {
+    Firebase.database()
+      .ref('news/')
+      .once('value')
+      .then(res => {
+        if (res.val()) {
+          const data = res.val();
+          const filterData = data.filter(el => el !== null)
+          setNews(filterData);
+        }
+      })
+      .catch(err => {
+        showError(err.message);
+      });
+  };
+
+  const getCategoryDoctor = () => {
+    Firebase.database()
+      .ref('category_doctor/')
+      .once('value')
+      .then(res => {
+        if (res.val()) {
+          const data = res.val();
+          const filterData = data.filter(el => el !== null)
+          setDoctorCtg(filterData);
+        }
+      })
+      .catch(err => {
+        showError(err.message);
+      });
+  };
+
+  const getRatedDoctor = () => {
+    Firebase.database()
+      .ref('doctors/')
+      .orderByChild('rate')
+      .limitToLast(3)
+      .once('value')
+      .then(res => {
+        console.log('doctor rated: ', res.val());
+        if (res.val()) {
+          const oldData = res.val();
+          const data = [];
+          Object.keys(oldData).map(key => {
+            data.push({
+              id: key,
+              data: oldData[key],
+            });
+          });
+          console.log('hasil parse data: ', data);
+          setDoctorRated(data);
+        }
+      })
+      .catch(err => {
+        showError(err.message);
+      });
+  };
+
   return (
     <View style={styles.page}>
       <View style={styles.container}>
         <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={styles.wrapperSection}>
-          <Gap height={30} />
-          <UserProfile onPress={()=>navigation.navigate('Profile')} />
-          <Gap height={30} />
-          <Text style={styles.titleOne}>
-            Mau konsultasi dengan siapa hari ini?
-          </Text>
-          <Gap height={16} />
-          <View style={styles.ctgContainer}>
-            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-              <View style={styles.ctgWrapper}>
-                <Gap width={16} />
-                {JSONCategoryDoctor.data.map(item => {
-                  return (
-                    <DoctorCategory key={item.id} category={item.category} onPress={()=>navigation.navigate('ChooseDoctor')} />
-                  )
-                })}
-              </View>
-            </ScrollView>
-          </View>
-          <Gap height={30} />
+          <View style={styles.wrapperSection}>
+            <Gap height={30} />
+            <UserProfile onPress={() => navigation.navigate('Profile')} />
+            <Gap height={30} />
+            <Text style={styles.titleOne}>
+              Mau konsultasi dengan siapa hari ini?
+            </Text>
+            <Gap height={16} />
+            <View style={styles.ctgContainer}>
+              <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                <View style={styles.ctgWrapper}>
+                  <Gap width={16} />
+                  {doctorCtg.map(item => {
+                    return (
+                      <DoctorCategory
+                        key={item.id}
+                        category={item.category}
+                        onPress={() => navigation.navigate('ChooseDoctor', item)}
+                      />
+                    );
+                  })}
+                </View>
+              </ScrollView>
+            </View>
+            <Gap height={30} />
             <Text style={styles.titleTwo}>Top Rated Doctor</Text>
           </View>
           <Gap height={16} />
           <View style={styles.wrapperSection}>
-            {JSONRatedDoctor.data.map(item =>{
+            {doctorRated.map(item => {
               return (
-                <DoctorRated key={item.id} name={item.name} category={item.category} onPress={()=>navigation.navigate('DoctorProfile')} />
-              )
+                <DoctorRated
+                  key={item.data.id}
+                  name={item.data.fullName}
+                  category={item.data.category}
+                  avatar={{uri: item.data.photo}}
+                  onPress={() => navigation.navigate('DoctorProfile', item)}
+                />
+              );
             })}
             <Gap height={14} />
             <Text style={styles.titleTwo}>Good News</Text>
           </View>
-          <NewsItem pic={DummyNews} title="Is it safe to stay at home during coronavirus?" />
-          <NewsItem pic={DummyNews2} title="Consume yellow citrus helps you healthier" />
-          <NewsItem pic={DummyNews3} title="Learn how to make a proper orange juice at home" />
+          {news.map(item => {
+            return (
+              <NewsItem
+                key={item.id}
+                pic={{uri: item.image}}
+                title={item.title}
+                date={item.date}
+              />
+            );
+          })}
           <Gap height={30} />
         </ScrollView>
       </View>
@@ -81,6 +164,7 @@ const styles = StyleSheet.create({
   },
   ctgContainer: {
     marginLeft: -16,
+    marginRight: -16,
   },
   container: {
     backgroundColor: colors.white,
