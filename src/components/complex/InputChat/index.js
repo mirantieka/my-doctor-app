a5b52995448f6790556a84cce4de3c7751074583
mirantieka <mirantiekaa4@gmail.com>
2021-09-29 10:42:34 +0700

    adding props and updating style InputChat component

--- src/components/complex/InputChat/index.js
+++ src/components/complex/InputChat/index.js
@@ -3,27 +3,28 @@ import {StyleSheet, Text, TextInput, View} from 'react-native';
 import { Button } from '../..';
 import { colors, fonts } from '../../../utils';
 
-export default function InputChat() {
+export default function InputChat({value, onChangeText, onPress}) {
   return (
     <View style={styles.container}>
-      <TextInput style={styles.input} placeholder="Tulis pesan untuk Nairobi" />
-      <Button type="btn-icon-send" disable  />
+      <TextInput style={styles.input(value)} placeholder="Tulis pesan disini" value={value} onChangeText={onChangeText} />
+      <Button type="btn-icon-send" disable={value.length < 1} onPress={onPress}  />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    input: {
+    input: value => ({
         backgroundColor: colors.input,
         borderRadius: 10,
         flex: 1,
         marginRight: 10,
         fontFamily: fonts.primary.normal,
         fontSize: 14,
-        color: colors.text.input,
-    },
+        color: value ? colors.text.primary : colors.text.input,
+        paddingLeft: 15,
+    }),
     container: {
         flexDirection: 'row',
-        padding: 16
+        padding: 16,
     }
 });
