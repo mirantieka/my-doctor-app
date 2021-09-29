53170fb8a821f7b8cf0e876678598515db598fde
mirantieka <mirantiekaa4@gmail.com>
2021-09-29 10:47:09 +0700

    intergration DoctorProfile page with firebase

--- src/pages/DoctorProfile/index.js
+++ src/pages/DoctorProfile/index.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { DummyDoctor } from '../../assets';
-import {
-  Button,
-  Gap,
-  Header,
-  ProfileItem
-} from '../../components/';
-import { ProfileDetail } from '../../components';
-import { colors } from '../../utils';
+import {StyleSheet, View} from 'react-native';
+import {DummyDoctor} from '../../assets';
+import {Button, Gap, Header, ProfileItem} from '../../components/';
+import {ProfileDetail} from '../../components';
+import {colors} from '../../utils';
 
-export default function DoctorProfile({navigation}) {
+export default function DoctorProfile({navigation, route}) {
+  const doctor = route.params;
   return (
     <View style={styles.page}>
-      <Header title="Profile" onPress={()=>navigation.goBack()} />
-      <ProfileItem name="Alexa Rachel" desc="Pediatrician" image={DummyDoctor} />
+      <Header title="Doctor Profile" onPress={() => navigation.goBack()} />
+      <ProfileItem
+        name={doctor.data.fullName}
+        desc={doctor.data.category}
+        image={{uri: doctor.data.photo}}
+      />
       <Gap height={26} />
-      <ProfileDetail label="Alumnus" value="Universitas Indonesia, 2020" />
-      <ProfileDetail label="Tempat Praktik" value="Rumah Sakit Umum, Bandung" />
-      <ProfileDetail label="No. STR" value="0000116622081996" />
+      <ProfileDetail label="Alumnus" value={doctor.data.university} />
+      <ProfileDetail
+        label="Tempat Praktik"
+        value={doctor.data.hospital_address}
+      />
+      <ProfileDetail label="No. STR" value={doctor.data.str_number} />
       <View style={styles.btn}>
-        <Button title="Start Consultation" onPress={()=>navigation.navigate('Chatting')} />
+        <Button
+          title="Start Consultation"
+          onPress={() => navigation.navigate('Chatting', doctor)}
+        />
       </View>
     </View>
   );
@@ -32,7 +38,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   btn: {
-      padding: 40,
-      paddingTop: 7
-  }
+    padding: 40,
+    paddingTop: 7,
+  },
 });
