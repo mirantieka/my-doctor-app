3bd5f26e1f4581a132d6b7ae6471179c0fcb4419
mirantieka <mirantiekaa4@gmail.com>
2021-09-29 10:45:48 +0700

    integration Chatting page with firebase

--- src/pages/Chatting/index.js
+++ src/pages/Chatting/index.js
@@ -1,24 +1,115 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import {ChatItem, Header, InputChat} from '../../components/complex';
-import {colors, fonts} from '../../utils';
+import {colors, fonts, getChatTime, setChatDate, showError} from '../../utils';
+import {getData} from '../../utils/localStorage';
+import {Firebase} from '../../config';
 
-export default function Chatting({navigation}) {
+export default function Chatting({navigation, route}) {
+  const doctor = route.params;
+  const [chatContent, setChatContent] = useState('');
+  const [user, setUser] = useState({});
+  const [chatData, setChatData] = useState([]);
+
+  useEffect(() => {
+    getUserFromLocalStorage();
+    const chatID = `${user.uid}_${doctor.data.uid}`;
+    const urlFirebase = `chatting/${chatID}/allChat/`;
+    Firebase.database()
+      .ref(urlFirebase)
+      .on('value', snapshot => {
+        if (snapshot.val()) {
+          const dataSnapshot = snapshot.val();
+          const allDataChat = [];
+
+          Object.keys(dataSnapshot).map(key => {
+            const dataChat = dataSnapshot[key];
+            const newDataChat = [];
+
+            Object.keys(dataChat).map(itemChat => {
+              newDataChat.push({
+                id: itemChat,
+                data: dataChat[itemChat],
+              });
+            });
+
+            allDataChat.push({
+              id: key,
+              data: newDataChat,
+            });
+          });
+          setChatData(allDataChat);
+        }
+      });
+  }, [doctor.data.uid, user.uid]);
+
+  const getUserFromLocalStorage = () => {
+    getData('user').then(res => {
+      setUser(res);
+    });
+  };
+
+  const chatSend = () => {
+    const today = new Date();
+
+    const data = {
+      sentBy: user.uid,
+      chatDate: today.getTime(),
+      chatTime: getChatTime(today),
+      chatContent: chatContent,
+    };
+
+    const chatID = `${user.uid}_${doctor.data.uid}`;
+
+    const urlFirebase = `chatting/${chatID}/allChat/${setChatDate(today)}`;
+
+    Firebase.database()
+      .ref(urlFirebase)
+      .push(data)
+      .then(() => {
+        setChatContent('');
+      })
+      .catch(err => {
+        showError(err.message);
+      });
+  };
+
   return (
     <View style={styles.page}>
       <Header
-        name="Nairobi Putri Hayza"
-        desc="Dokter Anak"
+        avatar={{uri: doctor.data.photo}}
+        name={doctor.data.fullName}
+        desc={doctor.data.category}
         type="header-profile"
-        onPress={()=>navigation.goBack()}
+        onPress={() => navigation.goBack()}
       />
       <View style={styles.container}>
-        <Text style={styles.chatDate}>Senin, 20 Maret 2021</Text>
-        <ChatItem isMe />
-        <ChatItem />
-        <ChatItem isMe />
+        <ScrollView>
+          {chatData.map(chat => {
+            return (
+              <View key={chat.id}>
+                <Text style={styles.chatDate}>{chat.id}</Text>
+                {chat.data.map(itemChat => {
+                  const isMe = itemChat.data.sentBy === user.uid
+                  return(
+                    <ChatItem 
+                    key={itemChat.id}
+                    isMe={isMe}
+                    text={itemChat.data.chatContent}
+                    time={itemChat.data.chatTime}
+                    avatar={isMe ? null : {uri: doctor.data.photo}} />
+                  )
+                })}
+              </View>
+            );
+          })}
+        </ScrollView>
       </View>
-      <InputChat />
+      <InputChat
+        value={chatContent}
+        onChangeText={value => setChatContent(value)}
+        onPress={chatSend}
+      />
     </View>
   );
 }
@@ -36,6 +127,6 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   container: {
-      flex: 1
-  }
+    flex: 1,
+  },
 });
