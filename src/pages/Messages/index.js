21bec3859641057f6fd500291f7bfe3719ee8187
mirantieka <mirantiekaa4@gmail.com>
2021-09-29 12:53:53 +0700

    integration Messages page with firebase

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -1,42 +1,66 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {DummyDoctor4, DummyDoctor5, DummyDoctor6} from '../../assets';
 import List from '../../components/complex/List';
+import {Firebase} from '../../config';
 import {colors, fonts} from '../../utils';
+import {getData} from '../../utils/localStorage';
 
 export default function Messages({navigation}) {
-  const [doctors, setDoctors] = useState([
-    {
-      id: 1,
-      pic: DummyDoctor4,
-      name: 'Alexander Jannie',
-      desc: 'Baik ibu, terima kasih banyak atas wakt...',
-    },
-    {
-      id: 2,
-      pic: DummyDoctor5,
-      name: 'Nairobi Putri Hayza',
-      desc: 'Oh tentu saja tidak karena jeruk it...',
-    },
-    {
-      id: 3,
-      pic: DummyDoctor6,
-      name: 'John McParker Steve',
-      desc: 'Oke menurut pak dokter bagaimana unt...',
-    },
-  ]);
+  const [user, setUser] = useState({});
+  const [historyChat, setHistoryChat] = useState([]);
+
+  useEffect(() => {
+    getUserFromLocalStorage();
+
+    const rootDB = Firebase.database().ref();
+    const urlMessageForUser = `messages/${user.uid}/`;
+    const messagesDB = rootDB.child(urlMessageForUser);
+
+    messagesDB.on('value', async snapshot => {
+      if (snapshot.val()) {
+        const oldData = snapshot.val();
+        const data = [];
+
+        const promises = Object.keys(oldData).map(async (key) => {
+          const urlUidDoctor = `doctors/${oldData[key].uidPartner}`;
+          const detailDoctor = await rootDB.child(urlUidDoctor).once('value');
+          console.log('detail doctor: ', detailDoctor.val());
+          data.push({
+            id: key,
+            detailDoctor: detailDoctor.val(),
+            ...oldData[key],
+          });
+        });
+        await Promise.all(promises);
+
+        console.log('new data history: ', data);
+        setHistoryChat(data);
+      }
+    });
+  }, [user.uid]);
+
+  const getUserFromLocalStorage = () => {
+    getData('user').then(res => {
+      setUser(res);
+    });
+  };
+
   return (
     <View style={styles.page}>
       <View style={styles.container}>
         <Text style={styles.title}>Messages</Text>
-        {doctors.map(doctor => {
+        {historyChat.map(chat => {
+          const doctor = {
+            id: chat.detailDoctor.uid,
+            data: chat.detailDoctor,
+          };
           return (
             <List
-              key={doctor.id}
-              pic={doctor.pic}
-              name={doctor.name}
-              desc={doctor.desc}
-              onPress={()=>navigation.navigate('Chatting')}
+              key={chat.id}
+              pic={{uri: chat.detailDoctor.photo}}
+              name={chat.detailDoctor.fullName}
+              desc={chat.lastContentChat}
+              onPress={() => navigation.navigate('Chatting', doctor)}
             />
           );
         })}
